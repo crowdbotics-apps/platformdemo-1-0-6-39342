8565beb1031197af66f0ca1c7ee0ca9d771e5e2a
shahraiz-cb <shahraiz.ali@crowdbotics.com>
2023-03-13 05:00:57 +0500

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- screens/about-the-app/index.js
+++ screens/about-the-app/index.js
@@ -1,23 +1,21 @@
-import React, { useState, useEffect } from "react";
+import { ImageBackground } from "react-native";
+import React from "react";
 import { Text, StyleSheet, View, SafeAreaView, Image } from "react-native";
 
-const AboutTheAppScreen = params => {
-  const [ImageSource, setImageSource] = useState();
-  const [text1, setText1] = useState("");
-  const [text2, setText2] = useState("");
-  useEffect(() => {
-    setText1("I understand that uses my dolor sit amet, consectetur adipiscing elit. Viverra auctor laoreet sodales congue sit volutpat quisque. Mattis nisl in convallis sed et. Est turpis aliquam est, ut mattis nisi, amet feugiat. Aliquet odio consequat, nisl mauris ullamcorper malesuada velit sem dolor. Dui morbi porttitor integer felis, pellentesque quam. Et accumsan justo, massa tincidunt arcu fermentum est. Sed nibh id vel, diam ut feugiat nec, placerat mauris. Neque lorem netus lacinia elit est libero sed. Commodo viverra et, neque augue augue mauris, nunc ut nec.");
-    setText2("I understand that uses my dolor sit amet, consectetur adipiscing elit. Viverra auctor laoreet sodales congue sit volutpat quisque. Mattis nisl in convallis sed et. Est turpis aliquam est, ut mattis nisi, amet feugiat. Aliquet odio consequat, nisl mauris ullamcorper malesuada velit sem dolor. Dui morbi porttitor integer felis, pellentesque quam. Et accumsan justo, massa tincidunt arcu fermentum est. Sed nibh id vel, diam ut feugiat nec, placerat mauris. Neque lorem netus lacinia elit est libero sed. Commodo viverra et, neque augue augue mauris, nunc ut nec.");
-    setImageSource(require("./assets/Frame21.png"));
-  }, []);
+const AboutTheAppScreen = ({
+  route
+}) => {
+  const {
+    item = {}
+  } = route.params || {};
   return <SafeAreaView style={styles.container}>
       <View style={styles.imgScroller}>
-        <Image source={ImageSource} />
+        
         <Image style={styles.threeDots} source={require("./assets/3Dots.png")} />
-      </View>
+      <ImageBackground style={styles.sSOevnzH} source={require("./Recipeful(2).png")} resizeMode="cover"></ImageBackground></View>
       <View style={styles.textContainer}>
-        <Text style={styles.text}>{text1}</Text>
-        <Text style={styles.text}>{text2}</Text>
+        <Text style={styles.text}>{item?.title}</Text>
+        <Text style={styles.text}>{item?.instructions}</Text>
       </View>
     </SafeAreaView>;
 };
@@ -45,6 +43,15 @@ const styles = StyleSheet.create({
     textAlign: "justify",
     lineHeight: 18,
     marginVertical: 10
+  },
+  sSOevnzH: {
+    width: 358,
+    height: 82,
+    position: "absolute",
+    left: -1,
+    top: -20,
+    flexDirection: "row",
+    flex: "1"
   }
 });
 export default AboutTheAppScreen;
